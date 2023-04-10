0a2880dec634d19fdda4bdd263e56f65384b1231
Manuel Saavedra <manuelsaavedrasoto@gmail.com>
2023-04-10 09:23:13 -0400

    Improved wall slide script Added wall slide animation

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,7 +7,9 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	if is_on_floor():
-		if not Input.is_anything_pressed():
+		if velocity.y == 0 and $Sprite.animation == "Fall":
+			$Sprite.stop()
+		if not $Sprite.is_playing():
 			$Sprite.play("Idle")
 		if Input.is_action_pressed("Right"):
 			velocity.x = SPEED
@@ -25,25 +27,36 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("Up"):
 			velocity.y += JUMP_VELOCITY
 			$Sprite.play("Jump")
+		if Input.is_action_just_pressed("Attack1"):
+			$Sprite.play("Attack")
+		if Input.is_action_just_released("Right") or Input.is_action_just_released("Left"):
+			$Sprite.stop()
 	if not is_on_floor():
 #		TODO Player slows if is attacking on a wall
-		if is_on_wall() and Input.is_action_pressed("Attack1"):
-			velocity.y = 10
-		velocity.y += delta * gravity
-		if -20<=velocity.y and velocity.y<=0:
-			$Sprite.play("Jump_peak")
-		if velocity.y > 0:
-			$Sprite.play("Fall")
-		if Input.is_action_pressed("Right"):
-			velocity.x = SPEED*.75
-			$Sprite.flip_h = false
-		elif Input.is_action_pressed("Left"):
-			velocity.x = -SPEED*.75
-			$Sprite.flip_h = true
+		if is_on_wall() and Input.is_action_pressed("Attack2"):
+			velocity.y = 0
+			velocity.x = 0
+			var collision = get_last_slide_collision()
+			$Sprite.play("WallSlide")
+			if collision.get_normal().x > 0:
+				$Sprite.flip_h = false
+			elif collision.get_normal().x < 0:
+				$Sprite.flip_h = true
 		else:
-			velocity.x = lerp(velocity.x, 0.0, .4)
-		
-	if Input.is_action_pressed("Attack1"):
-		$Sprite.play("Attack")
+			velocity.y += delta * gravity
+			if -20<=velocity.y and velocity.y<=0:
+				$Sprite.play("Jump_peak")
+			if velocity.y > 0:
+				$Sprite.play("Fall")
+			if Input.is_action_pressed("Right"):
+				velocity.x = SPEED*.75
+				$Sprite.flip_h = false
+			elif Input.is_action_pressed("Left"):
+				velocity.x = -SPEED*.75
+				$Sprite.flip_h = true
+			else:
+				velocity.x = lerp(velocity.x, 0.0, .4)
+			if Input.is_action_just_pressed("Attack1"):
+				$Sprite.play("Attack")
 	
 	move_and_slide()

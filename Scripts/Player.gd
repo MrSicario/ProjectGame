988581f578711cd815cc4ea34918be1c58e33c34
Manuel Saavedra <manuelsaavedrasoto@gmail.com>
2023-04-14 20:07:04 -0400

    Refactored movement script Added Pole jump

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,61 +2,94 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
+const ACCELERATION = .2
 const JUMP_VELOCITY = -400.0
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+const TOP_ANI = ["Attack", "WallSlide"]
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var clinging = false
+var spear_cd = true
+
+func get_Fall_Speed(delta, clinging):
+	if clinging and is_on_wall_only():
+		var collision = get_last_slide_collision()
+		velocity.y = 1
+		velocity.x = 0
+		if collision.get_normal().x > 0:
+			$Sprite.flip_h = false
+		else:
+			$Sprite.flip_h = true
+		if $Sprite.animation != "Attack":
+			$Sprite.play("WallSlide")
+	else:
+		velocity.y += gravity * delta
+		clinging = false
+
+func get_Input():
+	var direction = 0
+	if Input.is_action_pressed("Right"):
+		direction += 1
+	if Input.is_action_pressed("Left"):
+		direction -= 1
+	if direction != 0:
+		velocity.x = lerp(velocity.x, SPEED*direction, ACCELERATION)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, ACCELERATION)
+
+func play_Animation():
+	if not $Sprite.is_playing():
+		$Sprite.play("Idle")
+	if not $Sprite.animation in TOP_ANI:
+		if is_on_floor():
+			if $Sprite.animation == "Fall":
+				$Sprite.stop()
+			if Input.is_action_pressed("Right"):
+				$Sprite.flip_h = false
+				if velocity.x > 0:
+					$Sprite.play("Run")
+			if Input.is_action_pressed("Left"):
+				$Sprite.flip_h = true
+				if velocity.x < 0:
+					$Sprite.play("Run")
+			if Input.is_action_just_released("Right") or Input.is_action_just_released("Left"):
+				$Sprite.stop()
+		if not is_on_floor():
+			if velocity.y in range(-20, 20):
+				$Sprite.play("JumpPeak")
+			elif velocity.y > 0:
+				$Sprite.play("Fall")
+			elif velocity.y < 0:
+				$Sprite.play("Jump")
+
+func play_Attack():
+	$Sprite.stop()
+	$Sprite.play("Attack")
+
+func poleJump():
+	if (abs(velocity.x) > 350
+	and $RayCast2D.is_colliding()
+	and not is_on_floor()
+	and spear_cd):
+		velocity.y = JUMP_VELOCITY*1.5
+		spear_cd = false
+		
 func _physics_process(delta):
 	if is_on_floor():
-		if velocity.y == 0 and $Sprite.animation == "Fall":
+		clinging = false
+		spear_cd = true
+	get_Input()
+	get_Fall_Speed(delta, clinging)
+	if Input.is_action_just_pressed("Jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if clinging:
 			$Sprite.stop()
-		if not $Sprite.is_playing():
-			$Sprite.play("Idle")
-		if Input.is_action_pressed("Right"):
-			velocity.x = SPEED
-			$Sprite.flip_h = false
-			$Sprite.play("Run")
-		elif Input.is_action_pressed("Left"):
-			velocity.x = -SPEED
-			$Sprite.flip_h = true
-			$Sprite.play("Run")
-		if Input.is_action_pressed("Left") and Input.is_action_pressed("Right"):
-			velocity.x = 0
-			$Sprite.play("Idle")
-		else:
-			velocity.x = lerp(velocity.x, 0.0, .5)
-		if Input.is_action_just_pressed("Up"):
-			velocity.y += JUMP_VELOCITY
-			$Sprite.play("Jump")
-		if Input.is_action_just_pressed("Attack1"):
-			$Sprite.play("Attack")
-		if Input.is_action_just_released("Right") or Input.is_action_just_released("Left"):
-			$Sprite.stop()
-	if not is_on_floor():
-#		TODO Player slows if is attacking on a wall
-		if is_on_wall() and Input.is_action_pressed("Attack2"):
-			velocity.y = 0
-			velocity.x = 0
-			var collision = get_last_slide_collision()
-			$Sprite.play("WallSlide")
-			if collision.get_normal().x > 0:
-				$Sprite.flip_h = false
-			elif collision.get_normal().x < 0:
-				$Sprite.flip_h = true
-		else:
-			velocity.y += delta * gravity
-			if -20<=velocity.y and velocity.y<=0:
-				$Sprite.play("Jump_peak")
-			if velocity.y > 0:
-				$Sprite.play("Fall")
-			if Input.is_action_pressed("Right"):
-				velocity.x = SPEED*.75
-				$Sprite.flip_h = false
-			elif Input.is_action_pressed("Left"):
-				velocity.x = -SPEED*.75
-				$Sprite.flip_h = true
-			else:
-				velocity.x = lerp(velocity.x, 0.0, .4)
-			if Input.is_action_just_pressed("Attack1"):
-				$Sprite.play("Attack")
-	
+			clinging = false
+			velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("Attack1"):
+		play_Attack()
+		if is_on_wall_only():
+			clinging = true
+		poleJump()
+	play_Animation()
 	move_and_slide()
